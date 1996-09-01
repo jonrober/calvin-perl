6452e9c2b43ea50f5e68e8d9cda19643437772a7
Russ Allbery <rra@stanford.edu>
1996-09-01 17:39:37 +0000

    Added constants and a read method which actually parses server messages and tries to return something intelligent.  There are probably lots of bugs.

--- Calvin/Client.pm
+++ Calvin/Client.pm
@@ -17,12 +17,57 @@ use FileHandle;
 use Socket;
 
 use strict;
-use vars qw($ID $VERSION $BUFFER_SIZE);
+use vars qw(@ISA @EXPORT $ID $VERSION $BUFFER_SIZE);
 
-$ID      = '$Id$';
-$VERSION = (split (' ', $ID))[2];
+require Exporter;
+@ISA = qw(Exporter);
 
-$BUFFER_SIZE  = 256;		# Max size of the read buffer.
+$ID          = '$Id$';
+$VERSION     = (split (' ', $ID))[2];
+$BUFFER_SIZE = 256;
+
+@EXPORT = qw(C_PUBLIC C_POSE C_ROLL C_YELL C_YELL_POSE C_YELL_ROLL
+             C_WHIS C_WHIS_POSE C_WHIS_ROLL
+             C_CONNECT C_JOIN C_LEAVE C_NICKCHANGE C_SIGNOFF
+             C_E_NICK_USE C_E_NICK_LONG
+             C_UNKNOWN);
+
+
+#------------------------------  Constants  -------------------------------#
+
+# Public channel messages.
+sub C_PUBLIC         { 1 }	# Regular public channel messages.
+sub C_POSE           { 2 }	# Public poses.
+sub C_ROLL           { 3 }	# Public rolls.
+sub C_YELL           { 4 }	# Regular yells.
+sub C_YELL_POSE      { 5 }	# Yelled poses.
+sub C_YELL_ROLL      { 6 }	# Yelled rolls.
+
+# Private messages.
+sub C_WHIS           { 100 }	# Private whispers.
+sub C_WHIS_POSE      { 101 }	# Whispered poses.
+sub C_WHIS_ROLL      { 102 }	# Whispered rolls.
+
+# Server messages.
+sub C_CONNECT        { 200 }	# Connected to the chatserver.
+sub C_JOIN           { 201 }	# Joined channel.
+sub C_LEAVE          { 202 }	# Left channel.
+sub C_NICKCHANGE     { 203 }	# Changed nick.
+sub C_SIGNOFF        { 204 }	# Left the chatserver.
+
+# Error messages.
+sub C_E_NICK_LONG    { 1000 }	# Nick too long.
+sub C_E_NICK_USE     { 1001 }	# Nick already in use.
+sub C_E_NICK_INVALID { 1002 }	# Invalid nick.
+
+# Status messages.
+sub C_S_NICK         { 2000 }	# Initial response to nick setting.
+
+# Unknown messages.
+sub C_UNKNOWN        { 0 }	# Unknown message.
+
+
+#----------------------------  Public Methods  ----------------------------#
 
 # Create a new Calvin interface object.  In preparation of future revisions
 # of this module which will allow for multiple connections to be handled by
@@ -62,18 +107,18 @@ sub connect {
 	}
 
 	# We now have a nick prompt.  Send the nick.
-	$self->raw_send ("$nick\n");
+	$self->raw_send ("$self->{'nick'}\n");
 
 	# We should now see either the "nickname in use" message or the
 	# welcoming "you are now known as" message.  If we see "nickname in
 	# use", we need to call $fallback to change the nick and try again.
 	# Otherwise if we don't have a fallback or if we lost our
 	# connection, return undef.  Finally, if we've succeeded, return 1.
-	unless ($self->raw_read (\$buf)) {
+	$buf = $self->read;
+	if (!defined $buf) {
 	    $self->shutdown;
 	    return undef;
-	}
-	if ($buf =~ /^\Q*** Nickname \E"$nick"\Q in use./) {
+	} elsif ($buf != C_S_NICK) {
 	    if ($self->{'fallback'}) {
 		$self->{'nick'} = &{$self->{'fallback'}} ($self->{'nick'});
 		redo;
@@ -98,6 +143,77 @@ sub shutdown {
     $self->{'buffer'} = "";
 }
 
+# Read a line from the chatserver and try to parse it, returning one of the
+# following forms if successful:
+#
+#	C_PUBLIC, channel, user, message, on_channel
+#	C_POSE, channel, message, on_channel
+#	C_ROLL, channel, user, roll, on_channel
+#	C_YELL, user, message
+#	C_YELL_POSE, message
+#	C_YELL_ROLL, user, roll
+#
+#	C_WHIS, user, message
+#	C_WHIS_POSE, message
+#	C_WHIS_ROLL, user, roll
+#
+#	C_CONNECT, user, date, host
+#	C_JOIN, user, channel, topic
+#	C_LEAVE, user, channel, topic
+#	C_NICKCHANGE, olduser, newuser
+#	C_SIGNOFF, nick, reason, date
+#
+#       C_E_NICK_INVALID
+#	C_E_NICK_LONG
+#	C_E_NICK_USE
+#
+#       C_S_NICK
+#
+#	C_UNKNOWN, message
+#
+# That is in an array context.  In a scalar context, just returns the
+# message type.
+sub read {
+    my ($self) = @_;
+    my (@r);
+    local ($_);
+
+    # Grab a line of output.
+    unless ($self->raw_read (\$_)) { return undef }
+
+    # Try to parse it.
+    if    (/^<(\d+): (\S+)> (.*)/)          { @r = (C_PUBLIC, $1, $2, $3, 1) }
+    elsif (/^[(\d+): (\S+)] (.*)/)          { @r = (C_PUBLIC, $1, $2, $3, 0) }
+    elsif (/^\* (\d+): (.*)/)               { @r = (C_POSE, $1, $2, 1)       }
+    elsif (/^\* [(\d+): (.*)]$/)            { @r = (C_POSE, $1, $2, 0)       }
+    elsif (/^\#\# (\d+): (\S+) \S+ (.*)/)   { @r = (C_ROLL, $1, $2, $3, 1)   }
+    elsif (/^\#\# [(\d+): (\S+) \S+ (.*)]/) { @r = (C_ROLL, $1, $2, $3, 0)   }
+    elsif (/^[yell: (\S+)] (.*)/)           { @r = (C_YELL, $1, $2)          }
+    elsif (/^\* [yell: (.*)]$/)             { @r = (C_YELL_POSE, $1)         }
+    elsif (/^\#\# [yell: (\S+) \S+ (.*)]/)  { @r = (C_YELL_ROLL, $1, $2)     }
+    elsif (/^\*([^* ]|\S{2,})\* (.*)/)      { @r = (C_WHIS, $1, $2)          }
+    elsif (/^\*> (.*)/)                     { @r = (C_WHIS_POSE, $1)         }
+    elsif (/^\#\#> (\S+) \S+ (.*)/)         { @r = (C_WHIS_ROLL, $1, $2)     }
+    elsif (/^\*{3} Invalid nickname /)      { @r = (C_E_NICK_INVALID)        }
+    elsif (/^\*{3} Nickname \S+ too long/)  { @r = (C_E_NICK_LONG)           }
+    elsif (/^\*{3} Nickname \S+ in use/)    { @r = (C_E_NICK_USE)            }
+    elsif (/^\*{3} You are now known as /)  { @r = (C_S_NICK)                }
+    elsif (/^\*{3} (\S+) connected at (.*) from (\S+)\./)
+                                            { @r = (C_CONNECT, $1, $2, $3)   }
+    elsif (/^\*{3} (\S+) has joined channel (\d+) [(.*)]\./)
+                                            { @r = (C_JOIN, $1, $2, $3)      }
+    elsif (/^\*{3} (\S+) has left channel (\d+) [(.*)]\./)
+                                            { @r = (C_LEAVE, $1, $2, $3)     }
+    elsif (/^\*{3} (\S+) is now known as (\S+)\./)
+                                            { @r = (C_NICKCHANGE, $1, $2)    }
+    elsif (/^\*{3} Signoff: (\S+) \((.*)\) at (.*)\./)
+                                            { @r = (C_SIGNOFF, $1, $2, $3)   }
+    else                                    { @r = (C_UNKNOWN, $_)           }
+
+    # Return whatever we got.
+    return wantarray ? @r : @r[0];
+}
+    
 # Read from the file descriptor into the passed buffer until encountering
 # the passed delimiter.  If no delimiter is given, assume "\n".  The
 # delimiter is NOT included in the returned data.
@@ -108,8 +224,8 @@ sub raw_read {
 
     # Make sure our passed parameters are okay, and choose a delimiter to
     # look for.
-    unless ($buf)   { return undef  }
-    unless ($delim) { $delim = "\n" }
+    unless ($buf) { return undef  }
+    unless (defined $delim) { $delim = "\n" }
     $delim = quotemeta $delim;
 
     # Make sure we're connected.
@@ -158,6 +274,9 @@ sub raw_send {
     } until ($count == length $$buf);
     1;
 }
+
+
+#---------------------------  Private Methods  ----------------------------#
 
 # Open a TCP connection.
 sub tcp_connect {
