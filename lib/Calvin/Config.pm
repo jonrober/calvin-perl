d0998320dc9460602536779cd4b65e09fdbdcf68
Jon Robertson <jonrober@stanford.edu>
2018-01-21 14:11:25 -0800

    Update uberbot to use yaml config files
    Rewrite of how we do command handling for uberbot.  In the past we had a
    config file that was homegrown, then converted into a readable string that
    could also be used to configure additional bots from the bot connection
    itself.  This added parsing code and complexity that wasn't needed, and the
    feature of starting new bots from existing bot connections was never used.
    
    For better code cleanliness, that's all been pulled out and rewritten to a
    more standardized data structure and parsing handlers.  At the same time,
    there is now a command to update the old configuration file into a YAML
    file, to bring old bots over to the new uberbot.
    
    Closes #1

--- lib/Calvin/Config.pm
+++ lib/Calvin/Config.pm
@@ -13,6 +13,9 @@
 package Calvin::Config;
 require 5.002;
 
+use Perl6::Slurp;
+use YAML;
+
 use strict;
 use vars qw($host %bottypes);
 
@@ -35,130 +38,174 @@ $host = 'haven.eyrie.org';
 #  whether the error is fatal to creating a bot.  Prints error and dies if
 #  empty string (no specified user).  Otherwise msgs user with error string.
 sub send_error {
-    my ($client, $user, $error, $fatal) = @_;
+    my ($error, $fatal) = @_;
 
-    if    ($user ne '') { $client->msg($user, $error) }
-    elsif (!$fatal)     { print STDOUT $error."\n"    }
-    else                { die $error."\n"             }
+    if (!$fatal)     { print STDOUT $error."\n"    }
+    else             { die $error."\n"             }
 
     return $fatal;
 }
 
+############################################################################
+# Configuration handling.
+############################################################################
+
+# Given a string, determine if it's one that represents a boolean true or not,
+# returning 1 or 0.
+sub string_to_boolean {
+    my ($string) = @_;
+    return 1 if $string eq 'yes';
+    return 1 if $string eq 'true';
+    return 1 if $string eq '1';
+    return 0;
+}
+
+# Initialize any unset cambot fields to a default of undef, for setting
+# configuration values.  undef will be ignored by the setters, which will then
+# stay their defaults.
+sub clean_config {
+    my ($config, $fields, $bool_fields, $list_fields) = @_;
+
+    my %valid_fields = map { $_ => 1 } @$fields, @$bool_fields, @$list_fields;
+
+    # Complain if we find any unknown fields in the set.
+    for my $f (keys %{ $config} ) {
+        next if exists $valid_fields{$f};
+        my $error = "Invalid field: '$f'";
+        send_error('', '', $error, 1);
+    }
+
+    # Most fields we just set the default undef on if not set.
+    for my $f (@{ $fields }) {
+        $config->{$f} = undef unless exists $config->{$f};
+    }
+
+    # For boolean fields, also change the setting to 0/1.
+    for my $f (@{ $bool_fields }) {
+        if (exists $config->{$f}) {
+            $config->{$f} = string_to_boolean($config->{$f});
+        } else {
+            $config->{$f} = undef;
+        }
+    }
+
+    # Set any list fields to an empty array instead, since we'll be trying to
+    # iterate through at a higher level.
+    for my $f (@{ $list_fields }) {
+        $config->{$f} = [] unless exists $config->{$f};
+    }
+
+    return $config;
+}
+
+# Given a potential log directory, check to ensure that it is set up correctly
+# and has no problems.  Sends a fatal error if there are any non-warning
+# errors.
+sub cambot_logdir_check {
+    my ($logdir) = @_;
+
+    # Fail if the directory doesn't exist.
+    unless (-d $logdir) {
+        my $error = "Nonexistant log directory: '$logdir'.";
+        send_error('', '', $error, 1);
+    }
+
+    # Fail if the log directory isn't accessible.
+    unless (-x $logdir && -w $logdir) {
+        my $error = "Incorrect permissions: '$logdir'.";
+        send_error('', '', $error, 1);
+    }
+
+    # Get the mode to verify the logdir isn't accessible to others.
+    my ($mode) = (stat($logdir))[2];
+    $mode = $mode & 0777;
+    $mode = sprintf "%o", $mode;
+    if ($mode !~ /33$/) {
+        my $error = "Security warning: dir '$logdir' is ".
+            "accessible to others.";
+        send_error('', '', $error, 0);
+
+    # Also verify the parent of the logdir isn't accessible to others.
+    } else {
+        ($mode) = (stat($logdir."/.."))[2];
+        $mode = $mode & 0777;
+        $mode = sprintf "%o", $mode;
+        if ($mode !~ /11$/) {
+            my $error = "Security warning: parent of '$logdir' ".
+                "is accessible to others.";
+            send_error('', '', $error, 0);
+        }
+    }
+}
 
 ############################################################################
 # Bot creators
 ############################################################################
 
 sub make_cambot {
-    my ($client, $user, $config) = @_;
+    my ($config) = @_;
     my $cambot = new Calvin::Bots::Cambot;
-    my $error;
 
     # Defaults.
     $cambot->enable_recall(0);
 
-    # Settable configuration.
-    if ($config =~ s/\breadlog=(\d+)\b//)       { $cambot->max_readlog($1)   }
-    if ($config =~ s/\brecchan=(\d+)\b//)       { $cambot->max_recchan($1)   }
-    if ($config =~ s/\bbasename=(\S+)//)        { $cambot->basename($1)      }
-    if ($config =~ s/\bperm_channels='(.*?)'//) { $cambot->perm_channels($1) }
-    if ($config =~ s/\bmax_sessions=(\d+)\b//)  { $cambot->max_sessions($1)  }
-    if ($config =~ s/\bspamflag=(\S+)\b//)      { $cambot->spamflag($1)      }
+    # The fields we want to parse.
+    my @fields      = qw(readlog recchan basename perm_channels max_sessions
+                         spamflag recall_passwd);
+    my @bool_fields = qw(recall addtime usenumerics spam_after_invite);
+    my @list_fields = qw(autojoins);
 
-    while ($config =~ s/\bautojoin='(\d+), (.*?[^\\])'//) {
-        my ($chan, $tag) = ($1, $2);
+    # Settable configuration.
+    $config = clean_config($config, \@fields, \@bool_fields, \@list_fields);
+    $cambot->max_readlog($config->{readlog});
+    $cambot->max_recchan($config->{recchan});
+    $cambot->basename($config->{basename});
+    $cambot->perm_channels($config->{perm_channels});
+    $cambot->max_sessions($config->{max_sessions});
+    $cambot->spamflag($config->{spamflag});
+    $cambot->spam_after_invite($config->{spam_after_invite});
+    $cambot->use_numerics($config->{usenumerics});
+    $cambot->add_time($config->{addtime});
+    $cambot->enable_recall($config->{recall});
+
+    # Recall password both needs encryption, and will set enable_recall.
+    if (defined $config->{recall_passwd}) {
+        $cambot->enable_recall(1);
+        my $passwd = crypt($config->{recall_passwd}, 'jr');
+        $cambot->recall_passwd($passwd);
+    }
+
+    # Add any autojoin entries.
+    for my $chan (keys %{ $config->{autojoins}}) {
+        my $tag = $config->{autojoins}{$chan};
         $tag =~ s#\\'#'#;
         $cambot->autojoin($chan, $tag);
     }
-    if ($config =~ s/\brecall_passwd=(\S+)//) {
-        $cambot->enable_recall(1);
-        my $passwd = crypt($1, 'jr');
-        $cambot->recall_passwd($passwd);
-    }
-    if ($config =~ s/\brecall=(yes|no)\b//) {
-        if ($1 eq 'yes') { $cambot->enable_recall(1) }
-        else             { $cambot->enable_recall(0) }
-    }
-    if ($config =~ s/\baddtime=(yes|no)\b//) {
-        if ($1 eq 'yes') { $cambot->add_time(1) }
-        else             { $cambot->add_time(0) }
-    }
-    if ($config =~ s/\busenumerics=(yes|no)\b//) {
-        if ($1 eq 'yes') { $cambot->use_numerics(1) }
-        else             { $cambot->use_numerics(0) }
-    }
-    if ($config =~ s/\bspam_after_invite=(yes|no)\b//) {
-        if ($1 eq 'yes') { $cambot->spam_after_invite(1) }
-        else             { $cambot->spam_after_invite(0) }
-    }
 
-    if ($config =~ s/\blogdir='(.*?)'//) {
-        my $logdir = $1;
-        if (-d $logdir) {
-            if (-x $logdir && -w $logdir) {
-                my ($mode) = (stat($logdir))[2];
-                $mode = $mode & 0777;
-                $mode = sprintf "%o", $mode;
-                if ($mode !~ /33$/) {
-                    $error = "Security warning: dir '$logdir' is ".
-                        "accessible to others.";
-                    send_error($client, $user, $error, 0);
-                } else {
-                    ($mode) = (stat($logdir."/.."))[2];
-                    $mode = $mode & 0777;
-                    $mode = sprintf "%o", $mode;
-                    if ($mode !~ /11$/) {
-                        $error = "Security warning: parent of '$logdir' ".
-                            "is accessible to others.";
-                        send_error($client, $user, $error, 0);
-                    }
-                }
-                $cambot->logdir($logdir);
-            } else {
-                $error = "Incorrect permissions: '$logdir'.";
-                send_error($client, $user, $error, 1);
-                return undef;
-            }
-        } else {
-            $error = "Nonexistant log directory: '$logdir'.";
-            send_error($client, $user, $error, 1);
-            return undef;
-        }
-    } else {
-        $error = "You must specify a log directory for Cambot.";
-        send_error($client, $user, $error, 1);
-        return undef;
-    }
+    # Check the log directory is valid, dying if not.
+    my $logdir = $config->{logdir};
+    cambot_logdir_check($logdir);
+    $cambot->logdir($logdir);
 
-    # Make sure there are no bad config directives.
-    if ($config =~ /\S/) {
-        $config =~ s/^\s*(.*)\s*$/$1/;
-        $error = "Error in Cambot config: '$config'.";
-        send_error($client, $user, $error, 1);
-        return undef;
-    } else {
-        return $cambot;
-    }
+    return $cambot;
 }
 
 sub make_nagbot {
-    my ($client, $user, $config) = @_;
+    my ($config) = @_;
     my $nagbot = new Calvin::Bots::Nagbot;
 
-    # Make sure there are no bad config directives.  Nagbot doesn't
-    #  take any configurations.
-    if ($config =~ /\S/) {
-        $config =~ s/^\s*(.*)\s*$/$1/;
-        my $error = "Error in Nagbot config: '$config'.";
-        send_error($client, $user, $error, 1);
-        return undef;
-    } else {
-        return $nagbot;
-    }
+    # Nagbot has no configuration, but we still want to run it through the
+    # parser to make sure nothing has been sent that shouldn't have been.
+    my @fields      = qw();
+    my @bool_fields = qw();
+    my @list_fields = qw();
+    $config = clean_config($config, \@fields, \@bool_fields, \@list_fields);
+
+    return $nagbot;
 }
 
 sub make_standard {
-    my ($client, $user, $config) = @_;
+    my ($config) = @_;
     my $standard = new Calvin::Bots::Standard;
 
     # Default settings.
@@ -167,90 +214,69 @@ sub make_standard {
     $standard->quit_ok(1);
     $standard->renick_ok(1);
 
-    # Settable configuration.
-    if ($config =~ s/\bping=(yes|no)\b//) {
-        if ($1 eq 'yes') { $standard->ping_ok(1) }
-        else             { $standard->ping_ok(0) }
-    }
-    if ($config =~ s/\btime=(yes|no)\b//) {
-        if ($1 eq 'yes') { $standard->time_ok(1) }
-        else             { $standard->time_ok(0) }
-    }
-    if ($config =~ s/\bsay=(yes|no)\b//) {
-        if ($1 eq 'yes') { $standard->say_ok(1) }
-        else             { $standard->say_ok(0) }
-    }
-    if ($config =~ s/\brenick=(yes|no)\b//) {
-        if ($1 eq 'yes') { $standard->renick_ok(1) }
-        else             { $standard->renick_ok(0) }
-    }
+    # Normalize our fields.
+    my @fields      = qw();
+    my @bool_fields = qw(ping time say renick);
+    my @list_fields = qw();
+    $config = clean_config($config, \@fields, \@bool_fields, \@list_fields);
 
-    # Make sure there are no bad config directives.
-    if ($config =~ /\S/) {
-        $config =~ s/^\s*(.*)\s*$/$1/;
-        my $error = "Error in Standard config: '$config'.";
-        send_error($client, $user, $error, 1);
-        return undef;
-    } else {
-        return $standard;
-    }
+    # Make any requested settings.
+    $standard->ping_ok($config->{ping});
+    $standard->time_ok($config->{time});
+    $standard->say_ok($config->{say});
+    $standard->renick_ok($config->{renick});
+
+    return $standard;
 }
 
 sub make_descbot {
-    my ($client, $user, $config) = @_;
+    my ($config) = @_;
     my $descbot = new Calvin::Bots::Descbot;
-    my $error;
 
     # Default configuration.
     $descbot->localhost($host);
 
-    # Settable configuration.
-    if ($config =~ s/\bdescroot='(.*?)'//) {
-        if (-d $1 && -w $1) {
-            $descbot->descroot($1);
-        } else {
-            $error = "Bad desc root: '$1'.";
-            send_error($client, $user, $error, 1);
-            return undef;
-        }
-    } else {
-        $error = "You must specify a directory for Descbot.";
-        send_error($client, $user, $error, 1);
-        return undef;
+    # Normalize our fields.
+    my @fields      = qw(descroot);
+    my @bool_fields = qw();
+    my @list_fields = qw();
+    $config = clean_config($config, \@fields, \@bool_fields, \@list_fields);
+
+    # Error if no descroot set.
+    unless (defined $config->{descroot}) {
+        my $error = "You must specify a directory for Descbot.";
+        send_error('', , $error, 1);
     }
 
-    # Make sure there are no bad config directives.
-    if ($config =~ /\S/) {
-        $config =~ s/^\s*(.*)\s*$/$1/;
-        $error = "Error in Descbot config: '$config'.";
-        send_error($client, $user, $error, 1);
-        return undef;
-    } else {
-        return $descbot;
+    # Error if descroot isn't accessible.
+    unless (-d $config->{descroot} && -w $config->{descroot}) {
+        my $error = "Bad desc root: '$1'.";
+        send_error('', , $error, 1);
     }
+
+    $descbot->descroot($config->{descroot});
+
+    return $descbot;
 }
 
 sub make_fengroll {
-    my ($client, $user, $config) = @_;
+    my ($config) = @_;
 
     # Make bot and defaults.
     my $fengroll = new Calvin::Bots::Fengroll;
     $fengroll->roll_channel(25);
 
-    # Settable configuration.
-    if ($config =~ s/\broll_chan=(\d+)\b//) { $fengroll->roll_channel($1) }
-    if ($config =~ s/\bpasswd=(\S+)\b//)    { $fengroll->passwd($1)       }
+    # Normalize our fields.
+    my @fields      = qw(roll_chan passwd);
+    my @bool_fields = qw();
+    my @list_fields = qw();
+    $config = clean_config($config, \@fields, \@bool_fields, \@list_fields);
 
-    if ($config =~ /\S/) {
-        $config =~ s/^\s*(.*)\s*$/$1/;
-        my $error = "Error in Fengroll config: '$config'.";
-        send_error($client, $user, $error, 1);
-        return undef;
-    } else {
-        return $fengroll;
-    }
+    $fengroll->roll_channel($config->{roll_chan});
+    $fengroll->passwd($config->{passwd});
+
+    return $fengroll;
 }
-
 
 ############################################################################
 # Config makers
@@ -259,48 +285,41 @@ sub make_fengroll {
 # Send a manager, client, user, and argument line, then create a new bot
 #  using that configuration line.
 sub make_bot {
-    my ($client, $user, $args) = @_;
-    my ($nick, $server, $found_standard, $port, @connect, @bot_names);
-    $found_standard = 0;
+    my ($nick, $args) = @_;
+    my (@bot_names);
 
-    ($nick, $server, $port, $args) = split(/\s+/, $args, 4);
-    @connect = ($server, $port, $nick);
-    foreach my $bot (split(/;\s*/, $args)) {
-        my ($bot_type, $bot_args) = split(/\s+/, $bot, 2);
-        if (!defined $bot_args) { $bot_args = '' }
+    my $port   = $args->{port};
+    my $server = $args->{server};
+    my @connect = ($server, $port, $nick);
+
+    my $found_standard = 0;
+    foreach my $bot_type (keys %{ $args->{bots} }) {
         $bot_type = lc $bot_type;
-        my $newbot;
-
         $found_standard++ if $bot_type eq 'standard';
-        if (exists $bottypes{$bot_type}) {
-            $newbot = &{$bottypes{$bot_type}}($client, $user, $bot_args);
-        } else {
-            $newbot = undef;
-        }
 
-        if (defined $newbot) { push (@connect, $newbot) }
-        else                 { return undef             }
+        my $newbot = undef;
+        if (exists $bottypes{$bot_type}) {
+            $newbot = &{$bottypes{$bot_type}}($args->{bots}{$bot_type});
+        }
+        return undef unless defined $newbot;
+
+        push (@connect, $newbot);
         push (@bot_names, ucfirst($bot_type));
     }
 
     # If they didn't ask for a standard bot, tough.  Define the default
-    #  settings for one and add it in.
+    # settings for one and add it in.
     if (!$found_standard) {
-        my $newbot = make_standard ($client, $user, '');
+        my $newbot = make_standard ({});
         push (@connect, $newbot);
         push (@bot_names, 'Standard');
     }
 
     # Add the connection to the manager, make connect, initialize help,
-    #  and confirm for the user.  Add info string
-    my $connect_data;
-    if ($user ne '') {
-        $connect_data = "Bot $nick on $server $port by $user, using: ";
-        $client->msg ($user, "Bot $nick has been created for $server $port.");
-    } else {
-        $connect_data = "Bot $nick on $server $port by config file using: ";
-    }
+    # and confirm for the user.  Add info string
+    my $connect_data = "Bot $nick on $server $port by config file using: ";
     $connect_data .= join(', ', @bot_names).'.';
+
     push (@connect, $connect_data);
     return @connect;
 }
@@ -310,30 +329,14 @@ sub make_bot {
 #  make_bot, then sends each to make_bot to create the bots.
 sub config_rc {
     my ($fname) = @_;
-    my (@config, @newbots, $i);
-    $i = 0;
+    my (@config, @newbots);
 
-    open(CONFIG, $fname) ||
-        die "Error loading specified config file $fname: $!\n";
+    my $yaml_string = slurp($fname);
+    my $config = Load($yaml_string);
 
-    while (<CONFIG>) {
-        next unless /\S/;
-        s/^\s+//;
-        s/\s+$//;
-        tr/\r\n//d;
-        if (/^autojoin (.*)/) {
-            my $tag;
-            ($tag = $1) =~ s#'#\\'#;
-            $_ = "autojoin='$tag'";
-        }
-
-        if    (/^connect end/)               { $i++                         }
-        elsif (/^connect (\S+) (\S+) (\d+)/) { $config[$i] = "$1 $2 $3"     }
-        elsif (/^bot end/)                   { $config[$i] .= ';'           }
-        elsif (/^bot (\S+)/)                 { $config[$i] .= ' '.$1        }
-        else                                 { $config[$i] .= ' '.$_        }
+    for my $bot (keys %{ $config }) {
+        push @newbots, [ make_bot($bot, $config->{$bot}) ]
     }
-    foreach (@config) { push @newbots, [ make_bot('', '', $_) ] }
     return @newbots;
 }
 
